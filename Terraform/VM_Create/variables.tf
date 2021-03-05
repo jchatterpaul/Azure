ae76fcdfb83b7bbf78ff0b7d67bb36ea0411aa6e
Jay Chatterpaul <jchatterpaul@gmail.com>
2021-03-05 16:26:05 -0500

    organized better

---
+++ Terraform/VM_Create/variables.tf
@@ -0,0 +1,25 @@
+variable "subscriptionID" {
+  type        = string
+  description = "Subscription ID"
+}
+
+variable "tenantID" {
+  type = string
+}
+
+variable "location" {
+  type        = string
+  description = "Location"
+}
+
+variable "resourceGroupName" {
+  type = string
+}
+
+variable "networkName" {
+  type = string
+}
+
+variable "vmName" {
+  type = string
+}
